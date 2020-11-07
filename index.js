b7e808500730232054f4fbeeaf460480d56572ef
Dung Nguyen Dang <dangdungcntt@gmail.com>
2020-11-07 10:06:48 +0700

    Update packages

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const translate = require('@vitalets/google-translate-api');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors  = require('cors');
-const https = require('https');
-const fs = require('fs')
+require("dotenv").config();
+const translate = require("@vitalets/google-translate-api");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 const app = express();
 
 app.use(cors());
@@ -11,54 +12,59 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Express');
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to Express");
+});
 
-app.post('/vi2vi', (req, res) => {
-  console.log(req.body);
+app.post("/vi2vi", (req, res) => {
   if (!req.body.text) {
     return res.status(400).json({
-      msg: 'please send with "text" param'
+      msg: 'please send with "text" param',
     });
   }
   const text = req.body.text;
-  translate(text, { from: 'vi', to: 'vi' }).then(data => {
-    console.log(data);
-    if (data.from.text.didYouMean) {
+  translate(text, { from: "vi", to: "vi" })
+    .then((data) => {
+      if (data.from.text.didYouMean) {
+        return res.status(200).json({
+          text: data.from.text.value.split("[").join("").split("]").join(""),
+        });
+      }
       return res.status(200).json({
-        text: data.from.text.value.split('[').join('').split(']').join('')
+        text,
       });
-    }
-    return res.status(200).json({
-      text
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json({
+        msg: "Cannot get text",
+      });
     });
-  }).catch(err => {
-    console.log(err);
-    return res.status(400).json({
-      msg: 'Cannot get text'
-    });
-  });
-})
+});
 
 const port = process.env.PORT || 3000;
+const host = process.env.LISTEN_HOST || "0.0.0.0";
 
-if (process.env.NODE_ENV && process.env.NODE_ENV == 'production') {
-  app.listen(port, (err) => {
+if (process.env.APP_ENV == "production") {
+  app.listen(port, host, (err) => {
     if (err) {
       return console.log(err);
     }
-    console.log('RUNNING ON PORT ' + port)
+    console.log(`${process.env.APP_ENV} - RUNNING ON PORT ${host}:${port}`);
   });
-} else { 
+} else {
   const httpsOptions = {
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem')
-  }
-  https.createServer(httpsOptions, app).listen(port, (err) => {
+    key: fs.readFileSync("./key.pem"),
+    cert: fs.readFileSync("./cert.pem"),
+  };
+  https.createServer(httpsOptions, app).listen(port, host, (err) => {
     if (err) {
       return console.log(err);
     }
-    console.log('RUNNING ON PORT ' + port)
+    console.log(`${process.env.APP_ENV} - RUNNING ON PORT ${host}:${port}`);
   });
 }
+
+process.on("SIGINT", function () {
+  process.exit();
+});
